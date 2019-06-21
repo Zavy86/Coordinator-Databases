c831a83eebf80d022e3d77f7276e9e1533e0093b
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-06-21 09:29:53 +0200

    Module informations

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Databases
 modules/databases
-
---
+++ module.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Databases
+ *
+ * @package Coordinator\Modules\Databases
+ * @company Cogne Acciai Speciali s.p.a
+ * @authors Manuel Zavatta <manuel.zavatta@cogne.com>
+ */
+ // definitions
+ $module_name="databases";
+ $module_repository_url="https://github.com/Zavy86/Coordinator-Databases/";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Databases/master/VERSION.txt";
+ $module_required_modules=array();
+?>
