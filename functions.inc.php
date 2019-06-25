1f8f1a9a6400e63804773684a9fa14f97b974b20
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-06-25 09:34:35 +0200

    Functions

---
+++ functions.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Databases Functions
+ *
+ * @package Coordinator\Modules\Databases
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+
+ // include classes
+ require_once(DIR."modules/databases/classes/cDatabasesDatasource.class.php");
+
+?>
