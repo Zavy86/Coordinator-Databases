e387d62cb1725a8f74f497b4b4c920b1a907f984
Manuel Zavatta <manuel.zavatta@gmail.com>
2021-05-10 15:53:41 +0200

    View refactoring

--- datasources_view-informations.inc.php
+++ datasources_view-informations.inc.php
@@ -5,17 +5,19 @@
  * @package Coordinator\Modules\Databases
  * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
  * @link    http://www.coordinator.it
+ *
+ * @var strApplication $app
+ * @var cDatabasesDatasource $datasource_obj
  */
- // make informations description list
- $informations_dl=new strDescriptionList("br","dl-horizontal");
- $informations_dl->addElement(api_text("datasources_view-informations-dt-connector"),$datasource_obj->getConnector());
- if($datasource_obj->hostname){$informations_dl->addElement(api_text("datasources_view-informations-dt-hostname"),api_tag("samp",$datasource_obj->hostname));}
- if($datasource_obj->database){$informations_dl->addElement(api_text("datasources_view-informations-dt-database"),api_tag("samp",$datasource_obj->database));}
- if($datasource_obj->username){$informations_dl->addElement(api_text("datasources_view-informations-dt-username"),api_tag("samp",$datasource_obj->username));}
- if($datasource_obj->password){
-  if($_GET['password']){$informations_dl->addElement(api_text("datasources_view-informations-dt-password"),api_tag("samp",$datasource_obj->password));}
-  else{$informations_dl->addElement(api_text("datasources_view-informations-dt-password"),api_link("?mod=".MODULE."&scr=datasources_view&tab=informations&password=1&idDatasource=".$datasource_obj->id,api_tag("samp",str_repeat("*",strlen($datasource_obj->password)))));}
- }
- if($datasource_obj->tns){$informations_dl->addElement(api_text("datasources_view-informations-dt-tns"),api_tag("pre",$datasource_obj->tns));}
- if($datasource_obj->queries){$informations_dl->addElement(api_text("datasources_view-informations-dt-queries"),api_tag("pre",$datasource_obj->queries));}
-?>
+// make informations description list
+$informations_dl=new strDescriptionList("br","dl-horizontal");
+$informations_dl->addElement(api_text("cDatabasesDatasource-property-connector"),$datasource_obj->getConnector()->getLabel());
+if($datasource_obj->hostname){$informations_dl->addElement(api_text("cDatabasesDatasource-property-hostname"),api_tag("samp",$datasource_obj->hostname));}
+if($datasource_obj->database){$informations_dl->addElement(api_text("cDatabasesDatasource-property-database"),api_tag("samp",$datasource_obj->database));}
+if($datasource_obj->username){$informations_dl->addElement(api_text("cDatabasesDatasource-property-username"),api_tag("samp",$datasource_obj->username));}
+if($datasource_obj->password){
+	if($_GET['password']){$informations_dl->addElement(api_text("cDatabasesDatasource-property-password"),api_link(api_url(["scr"=>"datasources_view","tab"=>"informations","idDatasource"=>$datasource_obj->id,"password"=>false]),api_tag("samp",$datasource_obj->password)));}
+	else{$informations_dl->addElement(api_text("cDatabasesDatasource-property-password"),api_link(api_url(["scr"=>"datasources_view","tab"=>"informations","idDatasource"=>$datasource_obj->id,"password"=>true]),api_tag("samp",str_repeat("*",16))));}
+}
+if($datasource_obj->tns){$informations_dl->addElement(api_text("cDatabasesDatasource-property-tns"),api_tag("pre",$datasource_obj->tns));}
+if($datasource_obj->queries){$informations_dl->addElement(api_text("cDatabasesDatasource-property-queries"),api_tag("pre",$datasource_obj->queries));}
--- datasources_view.php
+++ datasources_view.php
@@ -5,43 +5,46 @@
  * @package Coordinator\Modules\Databases
  * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
  * @link    http://www.coordinator.it
+ *
+ * @var strApplication $app
  */
- // check authorizations
- api_checkAuthorization("databases-manage","dashboard");
- // get objects
- $datasource_obj=new cDatabasesDatasource($_REQUEST['idDatasource'],$_REQUEST['datasource']);
- // check objects  /** @todo decidere se usare exists o id */
- if(!$datasource_obj->exists()){api_alerts_add(api_text("databases_alert-datasource-exists"),"danger");api_redirect("?mod=".MODULE."&scr=datasources_list");}
- // deleted alert
- if($datasource_obj->deleted){api_alerts_add(api_text("datasources_view-alert-deleted"),"warning");}
- // include module template
- require_once(MODULE_PATH."template.inc.php");
- // set application title
- $app->setTitle(api_text("datasources_view",$datasource_obj->name));
- // check for tab
- if(!defined(TAB)){define("TAB","informations");}
- // build datasource description list
- $datasource_dl=new strDescriptionList("br","dl-horizontal");
- $datasource_dl->addElement(api_text("datasources_view-dt-name"),api_tag("strong",$datasource_obj->name));
- $datasource_dl->addElement(api_text("datasources_view-dt-typology"),$datasource_obj->getTypology());
- if($datasource_obj->description){$datasource_dl->addElement(api_text("datasources_view-dt-description"),nl2br($datasource_obj->description));}
- // build group table
- $table_groups=new strTable(api_text("datasources_view-tr-unvalued"));
- // include tabs
- require_once(MODULE_PATH."datasources_view-informations.inc.php");
- $tab=new strTab();
- $tab->addItem(api_icon("fa-flag-o")." ".api_text("datasources_view-tab-informations"),$informations_dl->render(),("informations"==TAB?"active":null));
- $tab->addItem(api_icon("fa-file-text-o")." ".api_text("datasources_view-tab-events"),api_events_table($datasource_obj->getLogs())->render(),("events"==TAB?"active":null));
- // build grid object
- $grid=new strGrid();
- $grid->addRow();
- $grid->addCol($datasource_dl->render(),"col-xs-12");
- $grid->addRow();
- $grid->addCol($tab->render(),"col-xs-12");
- // add content to application
- $app->addContent($grid->render());
- // renderize application
- $app->render();
- // debug
- api_dump($datasource_obj,"datasource");
-?>
+// check authorizations
+api_checkAuthorization("databases-usage","dashboard");
+// get objects
+$datasource_obj=new cDatabasesDatasource($_REQUEST["idDatasource"]);
+// check objects
+if(!$datasource_obj->exists()){api_alerts_add(api_text("cDatabasesDatasource-alert-exists"),"danger");api_redirect(api_url(["scr"=>"databases_list"]));}
+// deleted alert
+if($datasource_obj->deleted){api_alerts_add(api_text("cDatabasesDatasource-warning-deleted"),"warning");}
+// include module template
+require_once(MODULE_PATH."template.inc.php");
+// set application title
+$app->setTitle(api_text("datasources-view",$datasource_obj->name));
+// build databases description list
+$dl=new strDescriptionList("br","dl-horizontal");
+$dl->addElement(api_text("cDatabasesDatasource-property-name"),api_tag("strong",$datasource_obj->name));
+if($datasource_obj->description){$dl->addElement(api_text("cDatabasesDatasource-property-description"),nl2br($datasource_obj->description));}
+// check for tab
+if(!defined(TAB)){define("TAB","informations");}
+/**
+ * Informations
+ *
+ * @var strDescriptionList $informations_dl
+ */
+require_once(MODULE_PATH."datasources_view-informations.inc.php");
+// build tabs
+$tab=new strTab();
+$tab->addItem(api_icon("fa-flag-o")." ".api_text("datasources_view-tab-informations"),$informations_dl->render(),("informations"==TAB?"active":null));
+$tab->addItem(api_icon("fa-file-text-o")." ".api_text("datasources_view-tab-logs"),api_logs_table($datasource_obj->getLogs((!$_REQUEST["all_logs"]?10:null)))->render(),("logs"==TAB?"active":null));
+// build grid
+$grid=new strGrid();
+$grid->addRow();
+$grid->addCol($dl->render(),"col-xs-12");
+$grid->addRow();
+$grid->addCol($tab->render(),"col-xs-12");
+// add content to application
+$app->addContent($grid->render());
+// renderize application
+$app->render();
+// debug
+api_dump($datasource_obj,"datasource");
