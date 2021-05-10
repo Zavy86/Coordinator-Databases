d7bbcab2350aa2af5fc858dd3af09c49af0527a5
Manuel Zavatta <manuel.zavatta@gmail.com>
2021-05-10 15:54:45 +0200

    Datasource form

--- classes/cDatabasesDatasource.class.php
+++ classes/cDatabasesDatasource.class.php
@@ -70,6 +70,34 @@ class cDatabasesDatasource extends cObject{
 		return $this->name.($this->description?" (".$this->description.")":null);
 	}
 
+	/**
+	 * Edit form
+	 *
+	 * @param string[] $additional_parameters Array of url additional parameters
+	 * @return object Form structure
+	 */
+	public function form_edit(array $additional_parameters=array()){
+		// build form
+		$form=new strForm(api_url(array_merge(["mod"=>"databases","scr"=>"controller","act"=>"store","obj"=>"cDatabasesDatasource","idDatasource"=>$this->id],$additional_parameters)),"POST",null,null,"databases__datasource-edit_form");
+		// fields
+		$form->addField("text","name",api_text("cDatabasesDatasource-property-name"),$this->name,api_text("cDatabasesDatasource-placeholder-name"),null,null,null,"required");
+		$form->addField("textarea","description",api_text("cDatabasesDatasource-property-description"),$this->description,api_text("cDatabasesDatasource-placeholder-description"),null,null,null,"rows='2'");
+		$form->addField("select","typology",api_text("cDatabasesDatasource-property-typology"),$this->typology,api_text("cDatabasesDatasource-placeholder-typology"),null,null,null,"required");
+		foreach(cDatabasesDatasourceTypology::availables() as $typology_fobj){$form->addFieldOption($typology_fobj->code,$typology_fobj->text);}
+		$form->addField("select","connector",api_text("cDatabasesDatasource-property-connector"),$this->connector,api_text("cDatabasesDatasource-placeholder-connector"),null,null,null,"required");
+		foreach(cDatabasesDatasourceConnector::availables() as $connector_fobj){$form->addFieldOption($connector_fobj->code,$connector_fobj->text);}
+		$form->addField("text","hostname",api_text("cDatabasesDatasource-property-hostname"),$this->hostname,api_text("cDatabasesDatasource-placeholder-hostname"),null,"code");
+		$form->addField("text","database",api_text("cDatabasesDatasource-property-database"),$this->database,api_text("cDatabasesDatasource-placeholder-database"),null,"code");
+		$form->addField("text","username",api_text("cDatabasesDatasource-property-username"),$this->username,api_text("cDatabasesDatasource-placeholder-username"),null,"code");
+		$form->addField("text","password",api_text("cDatabasesDatasource-property-password"),$this->password,api_text("cDatabasesDatasource-placeholder-password"),null,"code");
+		$form->addField("textarea","tns",api_text("cDatabasesDatasource-property-tns"),$this->tns,api_text("cDatabasesDatasource-placeholder-tns"),null,"code",null,"rows='2'");
+		$form->addField("textarea","queries",api_text("cDatabasesDatasource-property-queries"),$this->queries,api_text("cDatabasesDatasource-placeholder-queries"),null,"code",null,"rows='2'");
+		// controls
+		$form->addControl("submit",api_text("form-fc-submit"));
+		// return
+		return $form;
+	}
+
 	// debug
 	//protected function event_triggered($event){api_dump($event);}
 
