40a5585121d8863b8e077ffbb1ec5ca3f044687b
Manuel Zavatta <manuel.zavatta@gmail.com>
2021-05-14 09:36:22 +0200

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,22 +5,22 @@
  * @package Coordinator\Modules\Databases
  * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
  * @link    http://www.coordinator.it
+ *
+ * @var strApplication $app
  */
 
- // include module template
- require_once(MODULE_PATH."template.inc.php");
- // set application title
- $app->setTitle(api_text("databases"));
- // build dashboard object
- $dashboard=new strDashboard();
- $dashboard->addTile("?mod=".MODULE."&scr=datasources_list",api_text("datasources_list"),api_text("datasources_list-description"),(api_checkAuthorization("databases-manage")),"1x1","fa-book");
- // build grid object
- $grid=new strGrid();
- $grid->addRow();
- $grid->addCol($dashboard->render(),"col-xs-12");
- // add content to application
- $app->addContent($grid->render());
- // renderize application
- $app->render();
-
-?>
+// include module template
+require_once(MODULE_PATH."template.inc.php");
+// set application title
+$app->setTitle(api_text("databases"));
+// build dashboard object
+$dashboard=new strDashboard();
+$dashboard->addTile("?mod=".MODULE."&scr=datasources_list",api_text("datasources_list"),api_text("datasources_list-description"),(api_checkAuthorization("databases-manage")),"1x1","fa-book");
+// build grid object
+$grid=new strGrid();
+$grid->addRow();
+$grid->addCol($dashboard->render(),"col-xs-12");
+// add content to application
+$app->addContent($grid->render());
+// renderize application
+$app->render();
